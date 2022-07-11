38f1c96ae7cf7e851b3df66a4daa6caaf82ad29c
MuhammadSaadJamil <muhammadsaadjamil1@gmail.com>
2022-07-12 00:34:23 +0500

    Added Admin settings

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Object
+
 
 # Register your models here.
+
+@admin.register(Object)
+class ObjectAdmin(admin.ModelAdmin):
+    search_fields = ['name']

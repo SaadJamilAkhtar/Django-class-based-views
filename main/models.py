e24c4002b26d843d1b2bbc0fa327477181505f5b
MuhammadSaadJamil <muhammadsaadjamil1@gmail.com>
2022-07-12 00:28:15 +0500

    Added models

--- main/models.py
+++ main/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Object(models.Model):
+    name = models.CharField(max_length=25, blank=False, null=False)
+    quantity = models.FloatField(default=0)
+
+    def __str__(self):
+        return self.name
